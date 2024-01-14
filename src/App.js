1ea3ed551c6d26b43f1394da1c8d0221a4a90e4f
Rajan Sharma <rajanraj8979@gmail.com>
2024-01-14 19:52:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import About from './component/About';
+import Contact from './component/Contact';
+import Home from './component/Home';
+import Main from './component/Main';
+import Product from './component/Product';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
+      <Main/>
+      <Product/>
+      <About/>
+      <Contact/>
     </div>
   );
 }
